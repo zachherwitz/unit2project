b76ce3ead90a797ee3799fdf32a05ae4a93ae862
Zach Herwitz <zach.herwitz@gmail.com>
2020-06-04 01:09:18 -0400

    added seed route

--- controllers/contacts_controller.js
+++ controllers/contacts_controller.js
@@ -17,7 +17,46 @@ const Contact = require('../models/contacts.js');
 
 // test route
 contacts.get('/', (req, res) => {
-  res.send('Testing, working!')
+  res.send('Testing, working now with seed route available!')
 })
+
+// testing seed route
+contacts.get('/seed/confirm', (req, res) => {
+  Contact.create(
+    [
+      {
+        name: 'Zach H',
+        department: 'Production',
+        title: 'Production Secretary',
+        departmentHead: false,
+        phone: 'XXX-XXX-XXXX',
+        email: 'XXXX@xxx.com',
+        emergencyContactName: 'Olivia M',
+        emergencyContactNumber: 'XXX-XXX-XXXX',
+        emergencyContactRelationship: 'Partner',
+        btl: true,
+        core: true
+      },
+      {
+        name: 'Max P',
+        department: 'Production',
+        title: 'Production Assistant',
+        departmentHead: false,
+        phone: 'XXX-XXX-XXXX',
+        email: 'XXXX@xxx.com',
+        btl: true,
+        core: true
+      }
+    ],
+    (error, data) => {
+      res.redirect('/contacts')
+    }
+  )
+})
+
+
+
+
+
 
 module.exports = contacts;
