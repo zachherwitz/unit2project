a410b6513df747cde64b9abff4f2a2c836f593f0
Zach Herwitz <zach.herwitz@gmail.com>
2020-06-09 08:37:01 -0400

    oop laptop gonna die

--- public/js/edit-contact.js
+++ public/js/edit-contact.js
@@ -1,19 +1,25 @@
 $(() => {
 
-  
+  $('.icon-input').on((event) => {
+    event.preventDefault();
+    if (event.code === 'Enter') {
+      console.log('enter pressed');
+    }
+  })
 
-  const addDistro = () => {
-    console.log('adding distro');
-    const $newDistroInput = $('<input>').addClass('distro-input')
-                                        .addClass('new-input')
-                                        .attr({'type': 'text', 'name': 'distros'})
-                                        .appendTo('.distros-container')
-  }
-
-  const $addDistroButton = $('<div>').addClass('add-distro-button')
-                                     .click(addDistro)
-                                     .appendTo('.distros-container')
-
+  //
+  // const addDistro = () => {
+  //   console.log('adding distro');
+  //   const $newDistroInput = $('<input>').addClass('distro-input')
+  //                                       .addClass('new-input')
+  //                                       .attr({'type': 'text', 'name': 'distros'})
+  //                                       .appendTo('.distros-container')
+  // }
+  //
+  // const $addDistroButton = $('<div>').addClass('add-distro-button')
+  //                                    .click(addDistro)
+  //                                    .appendTo('.distros-container')
+  //
 
 
 
